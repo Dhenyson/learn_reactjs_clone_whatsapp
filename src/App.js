e206b2907079acaf09297ed708c945d829543350
Dhenyson Jhean <dhenysonjhean@gmail.com>
2021-04-14 12:42:21 -0300

    Header do sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 export default () => {
   return (
@@ -6,7 +9,18 @@ export default () => {
       <div className="sidebar">
         
         <header>
-          ...
+          <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{color: '#919191'}} />
+            </div>
+          </div>
         </header>
 
         <div className="search">
