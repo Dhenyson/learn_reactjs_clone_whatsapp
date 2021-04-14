a1a4e1bfc9ca85dc98d48d4030220507c6fbf1a1
Dhenyson Jhean <dhenysonjhean@gmail.com>
2021-04-14 14:06:51 -0300

    lista de conversa

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
+import { useState } from 'react'
+
 import './App.css';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
 
+import ChatListItem from './components/ChatListItem'
+
 const colors = {
   icons: '#B1B3B5'
 }
 
 export default () => {
+  const [chatList, setChatlist] = useState([{},{}])
+
   return (
     <div className="app-window">
       <div className="sidebar">
-        
+
         <header>
           <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
           <div className="header--buttons">
             <div className="header--btn">
-              <DonutLargeIcon style={{color: colors.icons}} />
+              <DonutLargeIcon style={{ color: colors.icons }} />
             </div>
             <div className="header--btn">
-              <ChatIcon style={{color: colors.icons}} />
+              <ChatIcon style={{ color: colors.icons }} />
             </div>
             <div className="header--btn">
-              <MoreVertIcon style={{color: colors.icons}} />
+              <MoreVertIcon style={{ color: colors.icons }} />
             </div>
           </div>
         </header>
 
         <div className="search">
           <div className="search--input">
-            <SearchIcon fontSize="small" style={{color: colors.icons}} />
+            <SearchIcon fontSize="small" style={{ color: colors.icons }} />
             <input type="search" placeholder="Procurar ou começar uma nova conversa" />
           </div>
         </div>
 
-        <div className="chatList">
-          ...
+        <div className="chatlist">
+          {chatList.map((item, key) => (
+            <ChatListItem key={key} />
+          ))}
         </div>
 
       </div>
---
+++ src/components/ChatListItem.js
@@ -0,0 +1,22 @@
+import './ChatListItem.css'
+
+export default () => {
+    return (
+        <div className="chatListItem">
+            <img className="chatListItem--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
+            <div className="chatListItem--lines">
+                <div className="chatListItem--line">
+                    <div className="chatListItem--name">Dhenyson Jhean</div>
+                    <div className="chatListItem--date">13:20</div>
+                </div>
+                <div className="chatListItem--line">
+                    <div className="chatListItem--lastMsg">
+                        <p>Hello worldHello worldHello worlHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worlddHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello world
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
