64f0192afed7bc1eec0046f976f100dafa22a6b6
Dhenyson Jhean <dhenysonjhean@gmail.com>
2021-04-14 13:05:27 -0300

    mudando cores para dark

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+const colors = {
+  icons: '#B1B3B5'
+}
 
 export default () => {
   return (
@@ -12,19 +17,22 @@ export default () => {
           <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
           <div className="header--buttons">
             <div className="header--btn">
-              <DonutLargeIcon style={{color: '#919191'}} />
+              <DonutLargeIcon style={{color: colors.icons}} />
             </div>
             <div className="header--btn">
-              <ChatIcon style={{color: '#919191'}} />
+              <ChatIcon style={{color: colors.icons}} />
             </div>
             <div className="header--btn">
-              <MoreVertIcon style={{color: '#919191'}} />
+              <MoreVertIcon style={{color: colors.icons}} />
             </div>
           </div>
         </header>
 
         <div className="search">
-          ...
+          <div className="search--input">
+            <SearchIcon fontSize="small" style={{color: colors.icons}} />
+            <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+          </div>
         </div>
 
         <div className="chatList">
