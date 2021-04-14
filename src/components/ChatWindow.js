c0dd3a2f64af169d0cb5e558dbc9f2429fa0cef5
Dhenyson Jhean <dhenysonjhean@gmail.com>
2021-04-14 20:47:38 -0300

    Tela de conversa footer e input

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -2,6 +2,10 @@ import './ChatWindow.css'
 import SearchIcon from '@material-ui/icons/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import CloseIcon from '@material-ui/icons/Close';
+import SendIcon from '@material-ui/icons/Send';
+import MicIcon from '@material-ui/icons/Mic';
 
 import colors from '../colors'
 
@@ -34,6 +38,28 @@ export default () => {
 
             </div>
             <div className="chatWindow--footer">
+                <div className="chatWindow--pre">
+
+                    <div className="chatWindow--btn">
+                        <InsertEmoticonIcon style={{ color: colors.icons }} />
+                    </div>
+
+                </div>
+
+                <div className="chatWindow--inputarea">
+                    <input 
+                        className="chatWindow--input" 
+                        type="text"
+                        placeholder="Type a message"
+                    />
+                </div>
+                <div className="chatWindow--pos">
+
+                    <div className="chatWindow--btn">
+                        <SendIcon style={{ color: colors.icons }} />
+                    </div>
+
+                </div>
 
             </div>
         </div>
